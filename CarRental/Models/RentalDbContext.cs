4d23b04f1e08e14fbe6b267bf22673ddf4872f01
Hasan Alasker <hasanalasker2004@gmail.com>
2026-05-18 16:48:23 +0300

    DBContext semi structure

---
+++ CarRental/Models/RentalDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarRental.Models;
+
+public class RentalDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Rental> Rentals { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Transmission> Transmissions { get; set; }  
+    public DbSet<Type> Types { get; set; }
+    public DbSet<Fuel> Fuels { get; set; }
+    public DbSet<Roles> Roles { get; set; }
+    public DbSet<UserRoles> UserRoles { get; set; }
+
+    public RentalDbContext(DbContextOptions<RentalDbContext> options) : base(options)   
+    {
+            
+    }
+}
